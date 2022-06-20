5282df6d992a91768f2d00e3b57281f5832510c7
Joel Rosen <joel@velarium.com>
2022-06-20 16:57:11 -0600

    Selection Commit

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int favNumber = 7;
+
+            Console.WriteLine("Guess my favorite number");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < favNumber)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else if (userGuess > favNumber)
+            {
+                Console.WriteLine("Too High");
+            }
+            else if (userGuess == favNumber)
+            {
+                Console.WriteLine("YOU GOT IT!!");
+            }
+            else 
+            {
+                Console.WriteLine("nevermind");
+            }
+        }
+    }
+}
